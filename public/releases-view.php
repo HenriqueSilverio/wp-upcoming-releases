04476298ffef9c9cf929329d61dc321d4484d724
Henrique Silverio <henrique-silverio@hotmail.com>
2018-02-22 11:41:16 -0300

    Updating not found text - Closes #5

--- public/releases-view.php
+++ public/releases-view.php
@@ -16,7 +16,7 @@ $release_query = new WP_Query( $query );
 /**
  * Loop
  */
-if( $release_query->have_posts() ) : 
+if( $release_query->have_posts() ) :
 	while( $release_query->have_posts() ) : $release_query->the_post();
 		// Grab and define vars
 		global $post;
@@ -24,16 +24,16 @@ if( $release_query->have_posts() ) :
 		$release_title;
 		$classification;
 		$release_date;
-		
+
 		// Define values for vars
 		$cover = ( has_post_thumbnail() ) ? get_the_post_thumbnail( $post->ID, 'has_wpur_cover' ) : '';
-		
+
 		$release_title = get_the_title( $post->ID );
-		
+
 		$classification = get_post_meta( $post->ID, 'has_wpur_classification', true );
-		
+
 		$release_date = get_post_meta( $post->ID, 'has_wpur_release_date', true );
-		
+
 		if( has_term( '', 'has_wpur_category' ) ) {
 			$term_list     = wp_get_post_terms( $post->ID, 'has_wpur_category' );
 			$category_name = $term_list[0]->name;
@@ -57,11 +57,11 @@ if( $release_query->have_posts() ) :
 				<div class="has-wpur-box">
 					<figure class="has-wpur-box__cover">' . $cover . '</figure>
 					<span class="has-wpur-box__txt has-wpur-box__txt--title">' . $release_title . '</span>
-					<span class="has-wpur-box__title">' . __( 'Age Rating', 'wp-upcoming-releases' ) . '</span> 
+					<span class="has-wpur-box__title">' . __( 'Age Rating', 'wp-upcoming-releases' ) . '</span>
 					<span class="has-wpur-box__txt">' . $classification . '</span>
-					<span class="has-wpur-box__title">' . __( 'Category', 'wp-upcoming-releases' ) . '</span> 
+					<span class="has-wpur-box__title">' . __( 'Category', 'wp-upcoming-releases' ) . '</span>
 					<span class="has-wpur-box__txt">' . $category_name . '</span>
-					<span class="has-wpur-box__title">' . __( 'Release Date', 'wp-upcoming-releases' ) . '</span> 
+					<span class="has-wpur-box__title">' . __( 'Release Date', 'wp-upcoming-releases' ) . '</span>
 					<span class="has-wpur-box__txt">' . $release_date . '</span>
 				</div>
 			';
@@ -71,7 +71,7 @@ if( $release_query->have_posts() ) :
 		echo $output;
 	endwhile;
 else :
-	$cotent = __( 'Not found releases.', 'wp-upcoming-releases' );
+	$cotent = __( 'Nothing new for now. Stay tuned!', 'wp-upcoming-releases' );
 	echo '<p>' . $cotent . '<p>';
 endif;
 ?>
