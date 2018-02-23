d598c7c1efb2592d596cddd78b7e7b453b0e6b1b
Henrique Silverio <henrique-silverio@hotmail.com>
2018-02-23 13:28:20 -0300

    Refactor uninstall routine

--- uninstall.php
+++ uninstall.php
@@ -1,51 +1,34 @@
 <?php
 /**
- * Fired when the plugin is uninstalled.
+ * Plugin uninstall
  *
- * @package WP_Upcoming_Releases
- * @author Henrique Antonini Silvério <contato@henriquesilverio.com>
- * @license GPL-2.0+
- * @copyright 2013 Henrique Antonini Silvério
+ * Handles proper database clean up when plugin is deleted.
+ *
+ * @see https://developer.wordpress.org/plugins/the-basics/uninstall-methods/#method-2-uninstall-php
  */
 
 // If uninstall not called from WordPress, then exit
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit();
+if ( false === defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    die();
 }
 
-// Widget
-unregister_widget( 'has_wpur_widget' );
-
-// Custom Post Types and Attachments
+// Delete all `has_releases` posts
 $all_posts = get_posts( array( 'post_type' => 'has_releases', 'fields' => 'ids') );
 
 if( is_array( $all_posts ) ) {
-	foreach( $all_posts as $post ) {
-		$all_attachs = get_children( array( 'post_parent' => $post ) );
-
-		if( is_array( $all_attachs ) ) {
-			foreach( $all_attachs as $attach ) {
-				$attach_id = $attach->ID;
-				$file      = get_attached_file( $attach_id );
-
-				wp_delete_attachment( $attach_id, true );
-				unlink( $file );
-			}
-		}
-		
-		wp_delete_post( $post, true );
-	}
+    foreach( $all_posts as $post ) {
+        wp_delete_post( $post, true );
+    }
 }
 
-// Taxonomies and Terms					
-global $wp_taxonomies;
+// Delete all `has_wpur_category` terms
 $taxonomy = 'has_wpur_category';
 $terms    = get_terms( $taxonomy );
 
-if( $terms ) {
-	foreach ($terms as $term) {
-		wp_delete_term( $term->term_id, $taxonomy );
-	}
+if( is_array( $terms ) ) {
+    foreach ($terms as $term) {
+        wp_delete_term( $term->term_id, $taxonomy );
+    }
 }
-
-unset( $wp_taxonomies[ $taxonomy ] );
